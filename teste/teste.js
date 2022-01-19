9a6680c92d79f9569f39448b2ca9b2227b77956f
Cleber <kleberdejesus45@hotmail.com>
2022-01-19 10:24:13 -0300

    feat: teste de valor para ser colocado no cronometro

--- teste/teste.js
+++ teste/teste.js
@@ -4,13 +4,15 @@ function startTimer(duration, display) {
     minutes,
     seconds
   setInterval(function () {
-    hours = parseInt(timer / 60 / 60, 10)
+    days = parseInt(timer / 60 / 60 / 24, 10)
+    hours = parseInt((timer / 60 / 60) % 24, 10)
     minutes = parseInt((timer / 60) % 60, 10)
     seconds = parseInt(timer % 60, 10)
+    days = days < 10 ? '0' + days : days
     hours = hours < 10 ? '0' + hours : hours
     minutes = minutes < 10 ? '0' + minutes : minutes
     seconds = seconds < 10 ? '0' + seconds : seconds
-    display.textContent = hours + ':' + minutes + ':' + seconds
+    display.textContent = days + ':' + hours + ':' + minutes + ':' + seconds
     if (--timer < 0) {
       timer = duration
     }
