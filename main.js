454777f7a267035b2e63e5771346bbc3511a04a7
Cleber <kleberdejesus45@hotmail.com>
2022-01-20 16:03:12 -0300

    feat: aplicação do cronometro no site

--- main.js
+++ main.js
@@ -22,3 +22,38 @@ for (const link of links) {
     body.classList.remove('show')
   })
 }
+
+/* Cronometro */
+function startTimer(duration, nday, nhour, nminute, nsecond) {
+  var timer = duration,
+    hours,
+    minutes,
+    seconds
+  setInterval(function () {
+    days = parseInt(timer / 60 / 60 / 24, 10)
+    hours = parseInt((timer / 60 / 60) % 24, 10)
+    minutes = parseInt((timer / 60) % 60, 10)
+    seconds = parseInt(timer % 60, 10)
+    days = days < 10 ? '0' + days : days
+    hours = hours < 10 ? '0' + hours : hours
+    minutes = minutes < 10 ? '0' + minutes : minutes
+    seconds = seconds < 10 ? '0' + seconds : seconds
+    nday.textContent = days
+    nhour.textContent = hours
+    nminute.textContent = minutes
+    nsecond.textContent = seconds
+
+    if (--timer < 0) {
+      timer = duration
+    }
+  }, 1000)
+}
+window.onload = function () {
+  var duration = 60 * 58 * 25 * 88.3 // Converter para segundos
+  nDay = document.querySelector('#day .numero')
+  nHour = document.querySelector('#hour .numero')
+  nMinute = document.querySelector('#minute .numero')
+  nSecond = document.querySelector('#second .numero')
+
+  startTimer(duration, nDay, nHour, nMinute, nSecond) // iniciando o timer
+}
